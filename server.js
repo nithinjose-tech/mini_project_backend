c9b5f4299572153f4531723808f3ba36fa28e351
Jerry <jerrythomasjohn9@gmail.com>
2022-05-07 18:04:58 +0530

    Login changes

--- server.js
+++ server.js
@@ -31,11 +31,7 @@ mongoose
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 
 app.use(bodyParser.json());
 
