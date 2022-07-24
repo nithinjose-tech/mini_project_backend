92ed979255cf56504b4188462f2af56053c607de
Jerry <jerrythomasjohn9@gmail.com>
2022-07-24 10:11:25 +0530

    Categories update

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -2,115 +2,132 @@ const Products = require("../models/Products");
 const Orders = require("../models/Orders");
 const VendorStatistics = require("../models/VendorStatistics");
 
-
-exports.diplayProducts= async(req,res) =>{
-   
-
-     
-      
-      await Products.find().where(req.query.category?{ category:{"$regex":req.query.category,"$options": "i"} }:{}).where(req.query.name?{ $or:[{name:{"$regex":req.query.name,"$options": "i"} },{tags:{"$regex":req.query.name,"$options": "i"}}]}:{}).where(req.query.tags?{ tags: req.query.tags }:{}).sort({createdAt: 'desc'}).then((result)=>{
-        res.status(200).send(result)      
-      }).catch((err)=>{
-        res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving the product.",
-        });
-      })
-    
-}
-
-exports.viewCategories=async(req,res)=>{
-   const categoriers = ["Pottery","Digital","Paintings","Sculptures","Apparel","Used","Ornaments","Books","Crockery","Antiques","Utensils","Textiles"];
-   res.send(categoriers);
-}
-
-
-exports.findProductById = async(req,res) =>{
-        const id = req.params.id;
-
-        await Products.findById(id)
-          .then((data) => {
-            res.send(data);
-          })
-          .catch((err) => {
-            res.status(400).send({
-              message: "Product with given id not found",
-            });
-          });
- 
-}
-
-
-exports.purchaseProduct =async(req,res)=>{
-        const customerObject = JSON.parse(req.data);
-        console.log(`v_id:${customerObject._id}`)
-      if(customerObject.role=="CUSTOMER")
-
-      {
-        
-        try {
-            const purchase={
-              customer_id:customerObject._id,
-              items:req.body.items,
-              price:req.body.price,
-              paymentStatus:req.body.paymentStatus,
-              shipTo:req.body.shipTo
-
+exports.diplayProducts = async (req, res) => {
+  await Products.find()
+    .where(
+      req.query.category
+        ? { category: { $regex: req.query.category, $options: "i" } }
+        : {}
+    )
+    .where(
+      req.query.name
+        ? {
+            $or: [
+              { name: { $regex: req.query.name, $options: "i" } },
+              { tags: { $regex: req.query.name, $options: "i" } },
+            ],
           }
-
-          console.log(`items:${req.body.items.length}`)
-          
-
-          Orders.create(purchase)
-          .then((data) => {
-            res.send(data);
-          })
-          .catch((err) => {
-            res.status(500).send({
-              message:
-                err.message || "Some error occurred while creating the product.",
-            });
-          });
-
-         
-
-
-          for (let i = 0; i < req.body.items.length; i++) { 
-            // console.log(req.body.items[i].name);
-            Promise.all([
-              VendorStatistics.create({
-                vendor_id:req.body.items[i].vendor_id,
-                product_id:req.body.items[i].product_id,
-                name:req.body.items[i].name,
-                price:req.body.items[i].price,
-                quantity:req.body.items[i].quantity
-              }),
-              Products.findOneAndUpdate({_id :req.body.items[i].product_id}, {$inc : {'stock' : quantity*-1}})
-
-            ])
-            
-          }
-          
-        } catch (err) {
-          res.status(500).json(err);
-        }
-      }else{
-        res.status(401).send({
-             message:"Only CUSTOMERS can buy new products"
-        })
-    }
-       
-}
-
-
-exports.viewOrders = async(req,res)=>{
-  const customerObject = JSON.parse(req.data);
-  await Orders.find({ customer_id:customerObject._id }).sort({createdAt: 'desc'}).then((result)=>{
-    res.status(200).send(result)
-  }).catch((err)=>{
-    res.status(500).send({
-      message:
-        err.message || "Some error occurred while retrieving the product.",
+        : {}
+    )
+    .where(req.query.tags ? { tags: req.query.tags } : {})
+    .sort({ createdAt: "desc" })
+    .then((result) => {
+      res.status(200).send(result);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving the product.",
+      });
     });
-  })
-}
+};
+
+exports.viewCategories = async (req, res) => {
+  const categoriers = [
+    "Pottery",
+    "Paintings",
+    "Apparel",
+    "Used",
+    "Books",
+    "Crockery",
+    "Antiques",
+    "Utensils",
+    "Textiles",
+    "Handicrafts",
+    "Wood Crafts",
+    "Paper Crafts",
+    "Ornaments",
+  ];
+  res.send(categoriers);
+};
+
+exports.findProductById = async (req, res) => {
+  const id = req.params.id;
+
+  await Products.findById(id)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(400).send({
+        message: "Product with given id not found",
+      });
+    });
+};
+
+exports.purchaseProduct = async (req, res) => {
+  const customerObject = JSON.parse(req.data);
+  console.log(`v_id:${customerObject._id}`);
+  if (customerObject.role == "CUSTOMER") {
+    try {
+      const purchase = {
+        customer_id: customerObject._id,
+        items: req.body.items,
+        price: req.body.price,
+        paymentStatus: req.body.paymentStatus,
+        shipTo: req.body.shipTo,
+      };
+
+      console.log(`items:${req.body.items.length}`);
+
+      Orders.create(purchase)
+        .then((data) => {
+          res.send(data);
+        })
+        .catch((err) => {
+          res.status(500).send({
+            message:
+              err.message || "Some error occurred while creating the product.",
+          });
+        });
+
+      for (let i = 0; i < req.body.items.length; i++) {
+        // console.log(req.body.items[i].name);
+        Promise.all([
+          VendorStatistics.create({
+            vendor_id: req.body.items[i].vendor_id,
+            product_id: req.body.items[i].product_id,
+            name: req.body.items[i].name,
+            price: req.body.items[i].price,
+            quantity: req.body.items[i].quantity,
+          }),
+          Products.findOneAndUpdate(
+            { _id: req.body.items[i].product_id },
+            { $inc: { stock: quantity * -1 } }
+          ),
+        ]);
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(401).send({
+      message: "Only CUSTOMERS can buy new products",
+    });
+  }
+};
+
+exports.viewOrders = async (req, res) => {
+  const customerObject = JSON.parse(req.data);
+  await Orders.find({ customer_id: customerObject._id })
+    .sort({ createdAt: "desc" })
+    .then((result) => {
+      res.status(200).send(result);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving the product.",
+      });
+    });
+};
--- models/Products.js
+++ models/Products.js
@@ -2,29 +2,47 @@ const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true},
-    category: [{
-           type: String,
-           required: true,
-           enum:["Pottery","Digital","Paintings","Sculptures","Apparel","Used","Ornaments","Books","Crockery","Antiques","Utensils","Textiles"]
-    }],
-    vendor: { type: String, required: true},
-    vendor_id: { type: String, required: true},
-    stock: { type: Number, required: true},
-    price:{type:Number,required:true},
-    description:{type:String,required:true},
+    name: { type: String, required: true },
+    category: [
+      {
+        type: String,
+        required: true,
+        enum: [
+          "Pottery",
+          "Paintings",
+          "Apparel",
+          "Used",
+          "Books",
+          "Crockery",
+          "Antiques",
+          "Utensils",
+          "Textiles",
+          "Handicrafts",
+          "Wood Crafts",
+          "Paper Crafts",
+          "Ornaments",
+        ],
+      },
+    ],
+    vendor: { type: String, required: true },
+    vendor_id: { type: String, required: true },
+    stock: { type: Number, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true },
     // size:{
     //     type: String,
     //     required:true,
     //     enum: ["SM","MD","LG","XL","XXL"],
     // },
-    tags: [{
-        type: String
-    }],
-    image: { type: String,required:true },
-    rating:{
-        type:Number,
-        default:0
+    tags: [
+      {
+        type: String,
+      },
+    ],
+    image: { type: String, required: true },
+    rating: {
+      type: Number,
+      default: 0,
     },
   },
   { timestamps: true }
